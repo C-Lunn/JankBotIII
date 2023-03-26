f084ed651a84aeda97b1f7e7dbee9e9ecb55073e
leah <leah@southlondon.cc>
2023-03-26 13:40:46 +0100

    add some additional bits

--- utils/thread_scraper.ts
+++ utils/thread_scraper.ts
@@ -1,7 +1,16 @@
-import { Message, User } from "discord.js";
+import { Message, MessageEmbedAuthor, User } from "discord.js";
 import { Bot } from "../structs/Bot";
 
 const url_regex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[.\!\/\\w]*))?)/;
+
+type Song = {
+    url: string,
+    user: [string, string | null],
+    details: {
+        title: string | null | undefined,
+        author: MessageEmbedAuthor | null | undefined
+    }
+};
 
 export async function scrape_thread(bot: Bot, id: string) {
     const channel = bot.client.channels.cache.get(id);
@@ -12,7 +21,6 @@ export async function scrape_thread(bot: Bot, id: string) {
     //@ts-ignore
     const messages = await channel.messages.fetch({ limit: 100 });
 
-    type Song = { url: string, user: [string, string | null] };
     const data: Map<String, Song>[] = [
         new Map(),
         new Map(),
@@ -21,7 +29,7 @@ export async function scrape_thread(bot: Bot, id: string) {
     ];
 
     //@ts-ignore
-    messages.reverse().forEach(o => {
+    messages.reverse().forEach(async (o, i) => {
         const msg: Message = o;
         const matches = msg.content.match(url_regex);
         if (!matches) return;
@@ -29,19 +37,28 @@ export async function scrape_thread(bot: Bot, id: string) {
         // evil loop 
         let idx = 0;
         while (true) {
-            console.log(idx);
             if (idx == data.length) return;
             if (data[idx].get(msg.author.id)) {
                 idx++;
                 continue;
             }
+
+            const embed = msg.embeds[0];
+            let title, author;
+            if (embed) ({ title, author } = embed);
+
             data[idx].set(msg.author.id, {
                 url: matches[0],
+                details: {
+                    title, author
+                },
                 user: [msg.author.username, msg.author.avatar]
             });
+
             return;
-        }    
+        }
     });
+
 
     return data.map(o => Object.fromEntries(o));
 }
