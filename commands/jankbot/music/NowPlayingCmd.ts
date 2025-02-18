cba2c5150cce342757ba53b61735e2d1be199166
leah <leah@pronounmail.com>
2025-02-18 20:04:44 +0000

    aaa

--- commands/jankbot/music/NowPlayingCmd.ts
+++ commands/jankbot/music/NowPlayingCmd.ts
@@ -1,26 +1,25 @@
-import { i18n } from "../../../utils/i18n";
-import JankbotMusicCmd from "../../../interfaces/JankbotMusicCommand";
+import JankbotCmd, { JbMessage } from "../../../interfaces/JankbotCommand";
 import { Bot } from "../../../structs/Bot";
-import { JbMessage } from "../../../interfaces/JankbotCommand";
+import { i18n } from "../../../utils/i18n";
 
-export default class NowPlayingCmd extends JankbotMusicCmd {
+export default class NowPlayingCmd extends JankbotCmd {
     constructor(public bot: Bot) {
         super();
         this.name = "np";
         this.cooldown = 10;
         this.description = i18n.__("nowplaying.description");
     }
-    
+
     async run(bot: Bot, message: JbMessage) {
         const queue = bot.queues.get(message.guild!.id);
-        
+
         if (!queue || !queue.songs.length)
             return message.reply("No queue for this server.").catch(console.error);
-        
+
         const msg = await queue.generate_np_msg();
-        
+
         const mg = await message.channel.send({ embeds: [msg] });
-        
+
         queue.set_and_update_np_msg(mg);
     }
 }
