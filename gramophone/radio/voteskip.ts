ad03b2d6bbfc367a26f0b3a4a0084ddf05caf2e6
leah <leah@pronounmail.com>
2025-06-28 19:39:15 +0100

    add the thing

--- gramophone/radio/voteskip.ts
+++ gramophone/radio/voteskip.ts
@@ -2,7 +2,6 @@ import type { JbMessage } from "../../interfaces/JankbotCommand.ts";
 import JankbotMusicCmd from "../../interfaces/JankbotMusicCommand.ts";
 import { Bot } from "../../structs/Bot.ts";
 
-const voteskip_threshold = 5;
 const reactions = ["0️⃣", "1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"];
 const messages = [
     "The people have decided. This song fucking sucks dude.",
@@ -21,10 +20,14 @@ export default class VoteskipCmd extends JankbotMusicCmd {
     async run(bot: Bot, message: JbMessage) {
         if (!bot.radio_session) return message.reply("The radio is off.");
 
+        
         const song = bot.radio_session.queue.activeSong();
         song.voteskips.add(message.author.id);
         const members = bot.radio_session.stage.members.size - 1; // don't count the bot user
+
+        const voteskip_threshold = Math.ceil(Math.sqrt(members));
         const threshold = Math.min(voteskip_threshold, members);
+        
         message.react(reactions[Math.min(song.voteskips.size, voteskip_threshold)]);
 
         if (song.voteskips.size >= threshold) {
